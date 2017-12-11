475f0196dbfeb4a361a2c38bda42460635067cc3
Dávid Szabó <david.szabo97@gmail.com>
2017-12-11 17:52:30 +0100

    Add RecyclerView

--- app/src/main/java/com/messedcode/swipecollapseanimationexample/MainActivity.java
+++ app/src/main/java/com/messedcode/swipecollapseanimationexample/MainActivity.java
@@ -2,6 +2,7 @@ package com.messedcode.swipecollapseanimationexample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
     }
+
 }
