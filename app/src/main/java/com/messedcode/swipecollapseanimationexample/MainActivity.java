2a75ccf8bca9b00bfe29d9b7b1ec262cefa3b219
Dávid Szabó <david.szabo97@gmail.com>
2017-12-11 22:22:04 +0100

    Setup RecyclerView

--- app/src/main/java/com/messedcode/swipecollapseanimationexample/MainActivity.java
+++ app/src/main/java/com/messedcode/swipecollapseanimationexample/MainActivity.java
@@ -1,7 +1,8 @@
 package com.messedcode.swipecollapseanimationexample;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,6 +13,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         RecyclerView recyclerView = findViewById(R.id.recyclerView);
+
+        // Adapter
+        final CustomAdapter adapter = new CustomAdapter(new String[]{"Bread", "Egg", "Salmon", "Bacon", "Apple"});
+        recyclerView.setAdapter(adapter);
+
+        // Layout manager
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        recyclerView.setLayoutManager(layoutManager);
     }
 
 }
